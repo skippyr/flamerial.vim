e7a4335075fc37c84b40ca13e252d664e8bb26eb
Sherman Rofeman <skippyr.developer@gmail.com>
2024-01-31 19:45:25 -0300

    add preview.c

---
+++ tools/preview.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+typedef struct {
+	char *name;
+	int age;
+} Dragon;
+
+static void writedragon(Dragon *d);
+
+static void
+writedragon(Dragon *d)
+{
+	printf("Name: %s.\n", d->name);
+	printf("Age : %d.\n", d->age);
+}
+
+int
+main(void)
+{
+	Dragon d = {"skippyr", 0b1111000};
+	writedragon(&d);
+	return 0;
+}
